1f4c611f000c59316b53d1c55c2e9dc61369b6d5
GefryV3 <78043625+GefryV3@users.noreply.github.com>
2021-04-24 12:49:20 +0300

    Реализована функция выигрыша.

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import "./styles.css";
-
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
@@ -8,24 +7,28 @@ function Square(props) {
     </button>
   );
 }
-
 function Board(props) {
   const [xIsNext, setXIsNext] = useState(true);
   const [value, setValue] = useState(Array(9).fill(null));
-
   const handleClick = (i) => {
     const copyValue = value.slice();
+    if (calculateWinner(value) || value[i]) {
+      return;
+    }
     copyValue[i] = xIsNext ? "X" : "O";
     setXIsNext(!xIsNext);
     setValue(copyValue);
   };
-
   const renderSquare = (i) => {
     return <Square value={value[i]} onClick={() => handleClick(i)} />;
   };
-
-  const status = "Next player: X";
-
+  const winner = calculateWinner(value);
+  let status;
+  if (winner) {
+    status = "Выиграл " + winner;
+  } else {
+    status = "Следующий ход: " + (xIsNext ? "X" : "O");
+  }
   return (
     <div>
       <div className="status">{status}</div>
@@ -47,7 +50,6 @@ function Board(props) {
     </div>
   );
 }
-
 export function Game(props) {
   return (
     <div className="game">
@@ -60,4 +62,23 @@ export function Game(props) {
       </div>
     </div>
   );
+}
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
